92c2216622b1f6a6b251ec670a10b891fd004d0a
JesusBrz <jbruzonpimienta@gmail.com>
2020-10-18 14:21:37 -0500

    Server Logs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,12 @@ class ApplicationController < ActionController::Base
 
     before_action :configure_permitted_parameters, if: :devise_controller?
 
-    protected
-
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :surname])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :surname])
+        devise_parameter_sanitizer.permit(:sign_up) do |user|
+            user.permit(:name, :surname, :email, :password, :password_confirmation)
+        end
+        devise_parameter_sanitizer.permit(:account_update) do |user|
+            user.permit(:name, :surname, :email, :password, :password_confirmation)
+        end
     end
 end
