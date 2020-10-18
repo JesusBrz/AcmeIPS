c13ac7b1e955c9c3b766b8c2b45bfb4075b617db
JesusBrz <jbruzonpimienta@gmail.com>
2020-10-18 01:57:16 -0500

    Normal Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', registration: 'register', sign_up: 'signup' }
+  devise_for :users
   get 'citas/index'
   get 'home/index'
   
