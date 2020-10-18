944c8ad9b5364725038678c15578b2fe249fded5
JesusBrz <jbruzonpimienta@gmail.com>
2020-10-17 20:45:26 -0500

    Citas Controller

--- test/controllers/citas_controller_test.rb
+++ test/controllers/citas_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class CitasControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get citas_index_url
+    assert_response :success
+  end
+
 end
