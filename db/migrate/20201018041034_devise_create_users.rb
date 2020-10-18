4fc91978850727bdd7d1b9696c68f7377a1db96b
JesusBrz <jbruzonpimienta@gmail.com>
2020-10-17 23:57:25 -0500

    Translates and Config Devise

--- db/migrate/20201018041034_devise_create_users.rb
+++ db/migrate/20201018041034_devise_create_users.rb
@@ -14,6 +14,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Rememberable
       t.datetime :remember_created_at
 
+      ## Extra
+      t.string :name
+      t.string :surname
+
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
       # t.datetime :current_sign_in_at
