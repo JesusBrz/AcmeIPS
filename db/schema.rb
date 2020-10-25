cc221e30f196c5f1a3a5d3589fe4e99e653a2559
JesusBrz <jbruzonpimienta@gmail.com>
2020-10-25 16:41:37 -0500

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_18_233724) do
+ActiveRecord::Schema.define(version: 2020_10_25_212405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cita", force: :cascade do |t|
+    t.date "fecha"
+    t.time "hora"
+    t.string "tipo"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,6 +33,10 @@ ActiveRecord::Schema.define(version: 2020_10_18_233724) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "document"
+    t.date "birthday"
+    t.string "phone"
+    t.string "rol", default: "usuario"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
