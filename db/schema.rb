bb1766e468837c2c93b96e4caacd9d59cb30b05d
JesusBrz <jbruzonpimienta@gmail.com>
2020-10-26 21:46:11 -0500

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_26_001529) do
+ActiveRecord::Schema.define(version: 2020_10_27_000118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2020_10_26_001529) do
     t.string "nombre_medico"
     t.date "fecha"
     t.time "hora"
-    t.string "tipo"
+    t.string "tipo", default: "Web"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
