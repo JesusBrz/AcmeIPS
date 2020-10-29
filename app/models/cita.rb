156a014e34af840a03b873e8d96bd46e2725c081
JesusBrz <jbruzonpimienta@gmail.com>
2020-10-28 19:53:42 -0500

    Citas validation

--- app/models/cita.rb
+++ app/models/cita.rb
@@ -1,2 +1,6 @@
 class Cita < ApplicationRecord
+    validates :nombre_paciente, presence: true, length: { minimum: 5 }
+    validates :documento_paciente, presence: true, length: { minimum: 10 }
+    validates :telefono_paciente, presence: true, length: { minimum: 10 }
+    validates :nombre_medico, presence: true, length: { minimum: 5 }
 end
