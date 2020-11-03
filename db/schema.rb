3d365ae9f2fbf0370ac4f002f638ba346eb1e47d
JesusBrz <jbruzonpimienta@gmail.com>
2020-11-03 10:48:36 -0500

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_01_004731) do
+ActiveRecord::Schema.define(version: 2020_11_03_151603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 2020_11_01_004731) do
     t.date "fecha", null: false
     t.time "hora", null: false
     t.string "tipo", default: "Web", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "schedules", force: :cascade do |t|
+    t.date "day"
+    t.time "hour"
+    t.string "documento_paciente"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
