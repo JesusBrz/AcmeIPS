b570d10de9a02096841b8a2eb94fa55b9c5437c8
JesusBrz <jbruzonpimienta@gmail.com>
2020-11-05 15:35:44 -0500

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_03_151603) do
+ActiveRecord::Schema.define(version: 2020_11_05_200542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,8 +31,10 @@ ActiveRecord::Schema.define(version: 2020_11_03_151603) do
     t.date "day"
     t.time "hour"
     t.string "documento_paciente"
+    t.bigint "cita_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["cita_id"], name: "index_schedules_on_cita_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -55,4 +57,5 @@ ActiveRecord::Schema.define(version: 2020_11_03_151603) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "schedules", "citas"
 end
