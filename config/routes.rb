30a7e96ad932e67293ce595857d77496bdccea5a
JesusBrz <jbruzonpimienta@gmail.com>
2020-11-05 15:35:41 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  resources :schedules
   devise_for :users
   resources :citas
-  resources :schedules
   get 'home/index'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
