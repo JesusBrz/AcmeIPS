37b560b6b030fb62061add8755292dc70ace694b
JesusBrz <jbruzonpimienta@gmail.com>
2020-11-10 10:18:30 -0500

    Update Schema CRUD

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -71,6 +71,6 @@ class SchedulesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def schedule_params
-      params.require(:schedule).permit(:documento_paciente, :dia, :hora, :cita_id)
+      params.require(:schedule).permit(:documento_paciente, :documento_medico, :dia, :hora, :cita_id)
     end
 end
