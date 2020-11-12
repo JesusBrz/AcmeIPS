7a50907b6fdd8ac66c6e16a0eb9b138cdc5f693a
JesusBrz <jbruzonpimienta@gmail.com>
2020-11-12 08:42:34 -0500

    Delete Schedules

--- app/views/schedules/_schedule.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! schedule, :id, :documento_paciente, :documento_medico, :dia, :hora, :cita_id, :created_at, :updated_at
-json.url schedule_url(schedule, format: :json)
--- test/models/schedule_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class ScheduleTest < ActiveSupport::TestCase
-  test "should save Schedule" do
-    schedule = Schedule.new
-    assert true, "Schedule guardado con exito"
-  end
-end
