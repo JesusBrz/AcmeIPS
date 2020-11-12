c270a14d00baa3e6891d6515d924309d42b342b6
JesusBrz <jbruzonpimienta@gmail.com>
2020-11-12 08:46:50 -0500

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,35 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_143600) do
+ActiveRecord::Schema.define(version: 2020_11_01_004657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "citas", force: :cascade do |t|
-    t.string "nombre_paciente", null: false
-    t.string "documento_paciente", null: false
-    t.string "correo_paciente", null: false
-    t.string "telefono_paciente", null: false
-    t.string "nombre_medico", null: false
-    t.string "documento_medico", null: false
-    t.date "dia", null: false
-    t.time "hora", null: false
-    t.string "tipo", default: "web", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "schedules", force: :cascade do |t|
-    t.string "documento_paciente", null: false
-    t.string "documento_medico", null: false
-    t.date "dia", null: false
-    t.time "hora", null: false
-    t.bigint "cita_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["cita_id"], name: "index_schedules_on_cita_id"
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -60,5 +35,4 @@ ActiveRecord::Schema.define(version: 2020_11_10_143600) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "schedules", "citas"
 end
