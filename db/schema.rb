ba290b00f9299eb41af43fa137b174a6a9ff6bd3
JesusBrz <jbruzonpimienta@gmail.com>
2020-11-12 09:02:13 -0500

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_01_004657) do
+ActiveRecord::Schema.define(version: 2020_11_12_133632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "citas", force: :cascade do |t|
+    t.string "nombre_paciente", null: false
+    t.string "documento_paciente", null: false
+    t.string "correo_paciente", null: false
+    t.string "telefono_paciente", null: false
+    t.string "nombre_medico", null: false
+    t.date "dia", null: false
+    t.time "hora", null: false
+    t.string "tipo", default: "web", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
