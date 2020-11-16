befc53c3bf7fa6ce02bbc926860b4d9b942b161c
JesusBrz <jbruzonpimienta@gmail.com>
2020-11-16 12:44:47 -0500

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,19 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+  # Change Mail delivery to :smtp for Gmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'acmeips.com',
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+   }
 
   config.action_mailer.perform_caching = false
 
